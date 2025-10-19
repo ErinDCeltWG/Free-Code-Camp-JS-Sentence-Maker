7b2d057086a31f4774d75eb85bc0126aaa01cfc5
ErinDCeltWG <107307280+ErinDCeltWG@users.noreply.github.com>
2025-10-19 10:00:29 -0500

    Add files via upload

---
+++ Build a Sentence Maker.js
@@ -0,0 +1,25 @@
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+adjective = "silly";
+noun = "puca";
+noun2 = "curry";
+place = "fairy mound";
+adjective2 = "flaring";
+verb = "running";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+adjective = "troublesome";
+noun = "fairy";
+noun2 = "sushi";
+place = "fairy mound";
+adjective2 = "wee";
+verb = "sleeping";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
